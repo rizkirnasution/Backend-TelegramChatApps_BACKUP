6c5c60bd5ef553facf9f75b5a4e1f5fd21cfed91
Rizki Nasution <rizkirnasution@gmail.com>
2022-11-17 12:26:45 +0700

    fix auth login again

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -125,7 +125,7 @@ module.exports = {
       console.log(error)
       failed(res, {
         code: 500,
-        payload: error.message,
+        payload: error,
         message: 'Internal Server Error',
       });
     }
